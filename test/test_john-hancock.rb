3f95270738b8be53f8ee759fd71410f5198eb9b4
Aaron Scruggs <aascruggs@gmail.com>
2011-10-02 12:45:27 -0500

    tests

--- test/test_john-hancock.rb
+++ test/test_john-hancock.rb
@@ -1,7 +1,24 @@
 require 'helper'
 
 class TestJohnHancock < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+
+  should "sign hash" do
+    assert "2a9807b64b8068a8a2a665521ad9aeae", hash.sign
   end
+
+  should "sign hash with shared secret" do
+  	assert "d7321e9c8f961deb9e114dd476e420a0", hash.sign("ipsec")
+  end
+
+  should "sign hash with shared secret and ignore fields" do
+  	assert "b2d1e2808552c9d421b2b6f617c71012", hash.sign("ipsec", ["test"])
+  end
+
+  private 
+
+  def hash
+  	{"foo" => "bar", "test" => "toon"}
+  end
+
 end
